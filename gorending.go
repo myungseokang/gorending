cbaa4698a9555ed831dc2d40ec0e0a93885a2a52
Leop0ld <l3opold7@gmail.com>
2017-07-03 22:21:25 +0900

    대략적인 틀은 갖춤. html에서 파싱하는 로직만 작성하면 됨.

--- gorending.go
+++ gorending.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/urfave/cli"
@@ -9,62 +11,39 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "greet"
-	app.Usage = "fight the loneliness!"
+	app.Name = "gorending"
+	app.Usage = "Show Github trending in Terminal!"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "lang, l",
+			Value: "",
+			Usage: "language that you want",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello friend!")
+		lang := c.String("lang")
+
+		resp, err := http.Get("https://github.com/trending/" + lang)
+		if err != nil {
+			fmt.Println("http error is: ", err)
+			return err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("read error is: ", err)
+			return err
+		}
+
+		bodyStr := string(body)
+
+		fmt.Println(bodyStr)
+		fmt.Println("lang:", lang)
+
 		return nil
 	}
 
 	app.Run(os.Args)
 }
-
-const Version = "0.0.1"
-const Usage = `
-NAME:
-    gorending - Show github trending in Terminal!
-
-USAGE:
-    gorending [language]
-
-VERSION:
-    0.0.1
-
-COMMANDS:
-    help, h  Shows a list of commands or help for one command
-
-GLOBAL OPTIONS
-    --version Shows version information
-    --usage Shows usages
-`
-
-// func main() {
-// 	lang := flag.String("lang", "", "Language that you want")
-// 	version := flag.Bool("version", false, "Shows version information")
-// 	usage := flag.Bool("usage", false, "Shows usages")
-
-// 	flag.Parse()
-
-// 	if *version {
-// 		fmt.Println(Version)
-// 		os.Exit(0)
-// 	} else if *usage {
-// 		fmt.Println(Usage)
-// 		os.Exit(0)
-// 	}
-
-// 	resp, err := http.Get("https://github.com/trending/" + *lang)
-// 	if err != nil {
-// 		fmt.Println("http transport error is: ", err)
-// 	}
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println("read error is: ", err)
-// 	}
-
-// 	bodyStr := string(body)
-
-// 	fmt.Println(bodyStr)
-// 	fmt.Println("lang:", *lang)
-// }
