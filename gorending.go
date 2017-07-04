ddf8960715f495571bfe1b456de8607723dcfc87
Leop0ld <l3opold7@gmail.com>
2017-07-04 16:39:35 +0900

    Add getting trending data

--- gorending.go
+++ gorending.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"log"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/urfave/cli"
 )
+
+func CrawlTrending(lang string) error {
+	doc, err := goquery.NewDocument("https://github.com/trending/" + lang)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	doc.Find(".d-inline-block > h3 > a").Each(func(i int, s *goquery.Selection) {
+		repoName := strings.Trim(s.Text(), " \n")
+		fmt.Printf("%d - %s\n", i, repoName)
+	})
+	return nil
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "gorending"
 	app.Usage = "Show Github trending in Terminal!"
+	app.Version = "1.0.0"
+	app.Compiled = time.Now()
+	app.Copyright = "(c) 2017 Myungseo Kang"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Myungseo Kang",
+			Email: "l3opold7@gmail.com",
+		},
+	}
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -25,22 +50,10 @@ func main() {
 	app.Action = func(c *cli.Context) error {
 		lang := c.String("lang")
 
-		resp, err := http.Get("https://github.com/trending/" + lang)
+		err := CrawlTrending(lang)
 		if err != nil {
-			fmt.Println("http error is: ", err)
 			return err
 		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("read error is: ", err)
-			return err
-		}
-
-		bodyStr := string(body)
-
-		fmt.Println(bodyStr)
-		fmt.Println("lang:", lang)
 
 		return nil
 	}
