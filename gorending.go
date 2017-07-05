2f3b69e5df1dd5c7c7c569ca9deeb2c95b2e8342
Myungseo Kang <l3opold7@gmail.com>
2017-07-05 21:24:14 +0900

    Write more detail to README (#8)

--- gorending.go
+++ gorending.go
@@ -46,12 +46,12 @@ func main() {
 		cli.StringFlag{
 			Name:  "lang, L",
 			Value: "",
-			Usage: "language that you want to see, default all language",
+			Usage: "language that you want to see (default: all language)",
 		},
 		cli.IntFlag{
 			Name:  "count, C",
 			Value: 10,
-			Usage: "count that you want to see, defalut 10",
+			Usage: "count that you want to see",
 		},
 	}
 
