37a8f051abbf9455f92fb22ea20022d1d117c2ba
Myungseo Kang <myungseo.kang@opengallery.co.kr>
2018-04-18 14:56:45 +0900

    Add More interactive things using promptui

--- gorending.go
+++ gorending.go
@@ -1,14 +1,15 @@
 // Gorending is cli tool that crawls github trendings in Terminal at real time.
-package main
+package gorending
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/manifoldco/promptui"
+	"github.com/skratchdot/open-golang/open"
 	"github.com/urfave/cli"
 )
 
@@ -18,23 +19,79 @@ var GithubURL = "https://github.com"
 // TrendingURL is Github trending url
 var TrendingURL = GithubURL + "/trending/"
 
+// Repository Structure
+type Repository struct {
+	URL    string
+	Name   string
+	Author string
+}
+
 // CrawlTrending function requests to Github website and prints parsed trendings.
-func CrawlTrending(lang string, count int) error {
-	doc, err := goquery.NewDocument(TrendingURL + lang)
+func CrawlTrending(lang string, count int) []Repository {
+	doc, err := goquery.NewDocument(TrendingURL)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	rawRepoList := doc.Find(".d-inline-block > h3 > a").Slice(0, 10)
+
+	repoList := []Repository{}
+
+	rawRepoList.Each(func(i int, s *goquery.Selection) {
+		repoURL, ok := s.Attr("href")
+		if ok {
+			trendingName := strings.Trim(s.Text(), " \n")
+			trendingNameList := strings.Split(trendingName, " / ")
+			authorName := trendingNameList[0]
+			repoName := trendingNameList[1]
+
+			repo := Repository{
+				URL:    GithubURL + repoURL,
+				Name:   repoName,
+				Author: authorName,
+			}
+			repoList = append(repoList, repo)
+		}
+	})
+
+	return repoList
+}
+
+func ShowPrompt(repoList []Repository) error {
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "> {{ .Name | cyan }} ({{ .URL | red }})",
+		Inactive: "  {{ .Name | cyan }} ({{ .URL | red }})",
+		Selected: "! {{ .Name | red | cyan }}",
+		Details: `
+--------- Repository ----------
+{{ "URL:" | faint }}	 {{ .URL }}
+{{ "Name:" | faint }}	 {{ .Name }}
+{{ "Author: " | faint }}	 {{ .Author }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		repo := repoList[index]
+		name := strings.Replace(strings.ToLower(repo.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Repo",
+		Items:     repoList,
+		Templates: templates,
+		Size:      5,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
 		return err
 	}
 
-	repoList := doc.Find(".d-inline-block > h3 > a").Slice(0, count)
-
-	repoList.Each(func(i int, s *goquery.Selection) {
-		repoURL, ok := s.Attr("href")
-		repoName := strings.Trim(s.Text(), " \n")
-		if ok {
-			fmt.Printf("%d - %s (%s)\n", i+1, repoName, GithubURL+repoURL)
-		}
-	})
+	open.Run(repoList[i].URL)
 	return nil
 }
 
@@ -71,7 +128,9 @@ func main() {
 		lang := c.String("lang")
 		count := c.Int("count")
 
-		err := CrawlTrending(lang, count)
+		repoList := CrawlTrending(lang, count)
+		err := ShowPrompt(repoList)
+
 		if err != nil {
 			return err
 		}
